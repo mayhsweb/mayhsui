e3f7e970f7dabe21fbd033b8dc8cd4e12bee4438
srao-bit <srao.extern@europ-assistance.in>
2024-08-07 15:51:46 +0530

    new html

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+
+import brand from './images/m-logo.png'
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+        <div className='header'>
+        <img src={brand} />
+      </div>
+      
+    </div>
+  )
+}
+
+export default Header
